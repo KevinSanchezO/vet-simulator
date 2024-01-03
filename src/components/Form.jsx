0b851d23f0c4890503fc3999991d4d5d6095e9c9
Kevin Sánchez Obando <kesanchezo@gmail.com>
2024-01-03 16:59:32 -0600

    Started the implementation of hooks useState

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,29 @@
 // rfce
+import {useState} from 'react'
+
 function Form() {
+  const [petName, setPetName] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    //validations
+    if ([petName, ownerName, email, date, description].includes('')) {
+      setError(true);
+      return;
+    }
+    
+    setError(false);
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5 ">
       <h2 className="font-black text-3xl text-center">
         Seguimiento Pacientes
       </h2>
@@ -11,7 +33,17 @@ function Form() {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5"
+      >
+
+        {error && (
+          <div className='bg-red-600 text-lg text-center font-black mb-2 text-white p-3 rounded-md uppercase'>
+            <p>All fields are required</p>
+          </div>)
+        }
+
         <div className="mb-5">
           <label htmlFor="mascot" className="block text-gray-700 uppercase font-bold">
             Nombre de la mascota
@@ -21,6 +53,8 @@ function Form() {
             type="text"
             placeholder="Nombre de la mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400"
+            value={petName}
+            onChange={(event) => setPetName(event.target.value)}
           />
         </div>
 
@@ -33,6 +67,8 @@ function Form() {
             type="text"
             placeholder="Nombre del propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400"
+            value={ownerName}
+            onChange={(event) => setOwnerName(event.target.value)}
           />
         </div>
 
@@ -45,6 +81,8 @@ function Form() {
             type="email"
             placeholder="E-mail del propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
           />
         </div>
 
@@ -56,6 +94,8 @@ function Form() {
             id = 'doneDate'
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400"
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
           />
         </div>
 
@@ -67,6 +107,8 @@ function Form() {
             id="description"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400"
             placeholder="Describe los sintomas"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
           />
         </div>
 
